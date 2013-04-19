09fc09e4dbf50e1fb3e97714f04d39d6833538b2
Pawel Szczurko <pszczurko@gmail.com>
2013-04-18 23:46:08 -0400

    made security improvements

--- addFilter.php
+++ addFilter.php
@@ -1,9 +1,9 @@
 <?php
 require('/var/www/hackny/db.php');
 
-$filterType = $_POST['filter'];
-$word = $_POST['filter_word'];
-$userID = $_POST['user_id'];
+$filterType = mysql_real_escape_string($_POST['filter']);
+$word = mysql_real_escape_string($_POST['filter_word']);
+$userID = mysql_real_escape_string($_POST['user_id']);
 
 mysql_query("INSERT INTO filters VALUES (NULL, '$userID', '$filterType', '$word')");
 echo "<meta HTTP-EQUIV=\"REFRESH\" content=\"0; url=back.php\">";
--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 require('/var/www/hackny/db.php');
-$email = $_POST['username'];
-$pass = $_POST['password'];
-$num = $_POST['phone'];
+$email = mysql_real_escape_string($_POST['username']);
+$pass = mysql_real_escape_string($_POST['password']);
+$num = mysql_real_escape_string($_POST['phone']);
 
 mysql_query("INSERT INTO users VALUES (NULL,'$email', '$pass', '$num', '1')");
 echo "<meta HTTP-EQUIV=\"REFRESH\" content=\"0; url=login.php\">";
